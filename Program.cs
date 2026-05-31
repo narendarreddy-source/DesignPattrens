62179d752fb454f73aa57956d606319fb5a90641
Narendar Reddy <Narendarreddy16@gmail.com>
2026-05-30 22:26:27 -0500

    FlyWeight design pattren

--- Program.cs
+++ Program.cs
@@ -9,17 +9,8 @@ namespace Dsatest
         static void Main(string[] args)
         {
 
-            // Create subsystem objects
-            IPaymentService payment = new PaymentService();
-            IInventoryService inventory = new InventoryService();
-            IShippingService shipping = new ShippingService();
-            INotificationService notify = new NotificationService();
-
-            // Create Facade
-            IOrderService orderService = new Facade(payment, inventory, shipping, notify);
-
-            // Call Facade method
-            orderService.PlaceOrder();
+           Flyweight flyweight = new Flyweight();
+           
         }
     }
 }
---
+++ Structural/Flyweight.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesginPattrens.Structural
+{
+    public class Flyweight
+    {
+        public Flyweight() {
+
+            var sandlewood = TreeFactory.GetTreeType("SandleWood","Green","Smooth");
+            var tree1 = new Tree(10, 20, sandlewood);
+            var tree2 = new Tree(20, 10, sandlewood);
+        }
+    }
+
+    //Intrinsic shared data
+    public class TreeType
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Texture { get; set; }
+        public TreeType(string name,string color,string texture) {
+            this.Name = name;
+            this.Color = color;
+            this.Texture = texture;
+        }
+    }
+
+
+    //FlyWeight Factory
+    public class TreeFactory
+    {
+        private static readonly Dictionary<string, TreeType> _types = new Dictionary<string, TreeType>();
+        public static TreeType GetTreeType(string name, string color, string textture)
+        {
+            string key = $"{name}--{color}--{textture}";
+
+            if(!_types.ContainsKey(key))
+                _types[key] = new TreeType(name, color, textture);
+            return _types[key];
+        }
+    }
+
+
+    //Tree with extrinsic data
+    public class Tree
+    {
+        public int X { get; }
+        public int Y { get; }
+        public TreeType TreeType { get; }
+
+        public Tree(int x, int y, TreeType treeType)
+        {
+            X = x;
+            Y = y;
+            TreeType = treeType;
+        }
+    }
+}
